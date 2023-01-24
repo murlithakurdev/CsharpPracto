62b0b25b3e709e0b695f0bba67aaef9444df96d9
Murli Manohar Thakur <mthakur@ishir.com>
2023-01-24 14:45:52 +0530

    init

---
+++ CsharpPractice/Area.cs
@@ -0,0 +1,54 @@
+﻿using CsharpPractice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpPractice
+{
+    public class Area
+    {
+        public void Circle(int r) 
+        {
+            float pi = 3.14f;
+            float area = pi * r * r;
+            Console.WriteLine(area);
+        }
+        public void Circle(float r)
+        {
+            float p = 3.14f;
+            float area = p * r * r;
+            Console.WriteLine(area);
+        }
+        public void Circle()
+        {
+            int r = 6;
+            float pi = 3.14f;
+            float area = pi * r * r;
+            Console.WriteLine(area);
+        }
+
+    }
+    public class CircleArea : Area
+    {
+        public void Calculate(int r, float a)
+        {
+            Circle(r);
+            Circle(a);
+            Circle();
+        }
+    }
+
+    public class RectengularArea : Area
+    {
+        public void Calculate(int r, float a)
+        {
+            Circle(r);
+            Circle(a);
+            Circle();
+        }
+    }
+}
+
+
---
+++ CsharpPractice/Program.cs
@@ -0,0 +1,51 @@
+﻿using CsharpPractice;
+
+class Program
+{
+    public static void Main()
+    {
+        CircleArea obj = new CircleArea();
+        obj.Calculate(9);
+        //CircleArea
+        //AddNumber(5, 9);
+        // SubNumber(2, 9);
+        //printPiramidLeft(5);
+        //printPiramidRight(5);
+    }
+
+    public static void AddNumber(int a, int b)
+    {
+        int c = a + b;
+        Console.WriteLine(c);
+    }
+
+    public static void SubNumber(int a, int b)
+    {
+        int c = a - b;
+        Console.WriteLine(c);
+    }
+
+    static void printPiramidRight(int n) 
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void printPiramidLeft(int n)
+    {
+        for (int i = n; i < n; i++)
+        {
+            for (int j = n; j > i; j--)
+            {
+                Console.Write(j);
+            }
+            Console.WriteLine();
+        }
+    }
+}
